5b07630bf3c63ac6c8b6baae46d8a2e141335959
darkwight <m.alsharekh94@gmail.com>
2018-08-30 14:55:24 +0300

    Fixed errors + Added proper comments + Added runners

--- JSFoundationsProjectOne.js
+++ JSFoundationsProjectOne.js
@@ -1,6 +1,8 @@
-const numbers = [153,67,9,34,7,67,342,7,34,8656,2,124,5,43]
-
-// returns an array of just the even numbers from the received array.
+/**
+ * Receives an array of numbers
+ * Filters out odd numbers
+ * Returns an array of the even numbers
+ */
 function filterEvens(numbers) {
 	let evens = []
 	for(let i=0; i<numbers.length; i++) {
@@ -12,7 +14,11 @@ function filterEvens(numbers) {
 	return evens;
 }
 
-// returns an array of just the odd numbers from the received array.
+/**
+ * Receives an array of numbers
+ * Filters out even numbers
+ * Returns an array of the odd numbers
+ */
 function filterOdds(numbers) {
 	let odds = []
 	for(let i=0; i<numbers.length; i++) {
@@ -24,19 +30,26 @@ function filterOdds(numbers) {
 	return odds;
 }
 
+/**
+ * Receives an array of numbers
+ * Returns the sum of the odd numbers in that array
+ */
 function sumOdds(numbers) {
 	let odds = filterOdds(numbers);
 	let sum = 0;
-	for (let i=0; i<odd.length; i++) {
+	for (let i=0; i<odds.length; i++) {
 		sum += odds[i];
 	}
 	return sum;
 }
 
-// use multi line comments
-// makes the array into an array of pairs.
-// order doesn't matter.
-// if the length of the array is odd, the last element is in an array on its own.
+/**
+ * Receives an array of names
+ * Makes the array into an array of pairs
+ * Order of names and pairs doesn't matter
+ * if the length of the array is odd, the last element is in an array on its own
+ * Returns the array of pairs
+ */
 function makePairs(names) {
 	let result = [];
 	let pairs = [];
@@ -55,5 +68,20 @@ function makePairs(names) {
 	return result;
 }
 
-// have runners for all the functions, with a message that introduces this block of the code, and not explicitly say to not touch the code.
-console.log(makePairs(["Hamza", "Mshary", "Aziz", "Hussein", "Fawaz"]))
+/**************************************************
+The following code runs the functions defined above
+***************************************************/
+let numbers = [153,67,9,34,7,67,342,7,34,8656,2,124,5,43];
+let instructors = ["Hamza", "Mshary", "Aziz", "Hussein", "Fawaz"];
+
+// filterEvens
+evens = filterEvens(numbers);
+console.log(evens);
+
+// sumOdds
+sum = sumOdds(numbers);
+console.log(sum);
+
+// makePairs
+let pairs = makePairs(instructors);
+console.log(pairs);
