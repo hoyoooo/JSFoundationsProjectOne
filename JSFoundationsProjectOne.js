256e3d200bb1cfc7d9184ffc3b1f40b8596317ca
darkwight <m.alsharekh94@gmail.com>
2018-09-03 14:48:30 +0300

    finalized the starting point code

--- JSFoundationsProjectOne.js
+++ JSFoundationsProjectOne.js
@@ -2,6 +2,12 @@
  * Receives an array of numbers
  * Filters out odd numbers
  * Returns an array of the even numbers
+ *
+ * Example usage:
+ *
+ *     filterEvens([1, 2, 3, 4, 5])
+ *     returns [2, 4]
+ *
  */
 function filterEvens(numbers) {
 	// your code goes here!
@@ -11,6 +17,12 @@ function filterEvens(numbers) {
  * Receives an array of numbers
  * Filters out even numbers
  * Returns an array of the odd numbers
+ *
+ * Example usage:
+ *
+ *     filterOdds([1, 2, 3, 4, 5])
+ *     returns [1, 3, 5]
+ *
  */
 function filterOdds(numbers) {
 	// your code goes here!
@@ -19,6 +31,12 @@ function filterOdds(numbers) {
 /**
  * Receives an array of numbers
  * Returns the sum of the odd numbers in that array
+ *
+ * Example usage:
+ *
+ *     sumOdds([1, 2, 3, 4, 5])
+ *     returns 9
+ *
  */
 function sumOdds(numbers) {
 	// your code goes here!
@@ -30,6 +48,15 @@ function sumOdds(numbers) {
  * Order of names and pairs doesn't matter
  * if the length of the array is odd, the last element is in an array on its own
  * Returns the array of pairs
+ *
+ * Example usage:
+ * 
+ *     pairs(['Asis', 'Hamsa', 'Fawas', 'Mishmish', 'Hussein'])
+ *     returns [['Mishmish', 'Fawas'], ['Asis', 'Hussein'], ['Hamsa']]
+ * 
+ *     pairs(['Aziz', 'Hamza', 'Fawaz', 'Mishmish'])
+ *     returns [['Mishmish', 'Fawaz'], ['Aziz', 'Hamza']]
+ * 
  */
 function makePairs(names) {
 	// your code goes here!
@@ -41,18 +68,15 @@ The following code runs the functions defined above
 let numbers = [153,67,9,34,7,67,342,7,34,8656,2,124,5,43];
 let instructors = ["Hamza", "Mshary", "Aziz", "Hussein", "Fawaz"];
 
-// filterEvens
-evens = filterEvens(numbers);
-console.log(evens);
+// uncomment the following lines to run and test your code.
+// // filterEvens
+// evens = filterEvens(numbers);
+// console.log(evens);
 
-// sumOdds
-sum = sumOdds(numbers);
-console.log("\n--------------------------")
-console.log("SUM ODDS:");
-console.log(sum);
+// // sumOdds
+// sum = sumOdds(numbers);
+// console.log(sum);
 
-// makePairs
-let pairs = makePairs(instructors);
-console.log("\n--------------------------")
-console.log("MAKE PAIRS:");
-console.log(pairs);
+// // makePairs
+// let pairs = makePairs(instructors);
+// console.log(pairs);
